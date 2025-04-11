69a47b5848fec5cec23465854ecafde0e1baa11f
Tim Macdonald <macdonald.tim@gmail.com>
2025-04-11 10:21:25 -0400

    Basic input

--- src/main.js
+++ src/main.js
@@ -1,24 +1,49 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+const winners = new Set();
+
+document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <form>
+      <input type="number" name="candidate" />
+    </form>
+    <div id="status"></div>
+    <div class="buttons">
+      <input type="button" value="1" />
+      <input type="button" value="2" />
+      <input type="button" value="3" />
+      <input type="button" value="4" />
+      <input type="button" value="5" />
+      <input type="button" value="6" />
+      <input type="button" value="7" />
+      <input type="button" value="8" />
+      <input type="button" value="9" />
+      <input type="button" value="0" />
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+const form = document.querySelector("form");
+const status = document.querySelector("#status");
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  const candidate = form.elements.candidate.value;
+  if (winners.has(candidate)) {
+    status.innerHTML = "already won";
+  } else {
+    winners.add(candidate);
+    status.innerHTML = "new winner";
+  }
+});
+
+const numberButtons = document.querySelectorAll(
+  '.buttons > input[type="button"]'
+);
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (ev) => {
+    const number = ev.target.value;
+    const candidate = form.elements.candidate.value;
+    form.elements.candidate.value = candidate + number;
+  });
+});
