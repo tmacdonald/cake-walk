2ccf6944ec18f32466f7f91053778f12c62d1a79
Tim Macdonald <macdonald.tim@gmail.com>
2025-04-11 22:22:02 -0400

    base functionality

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import "./style.css";
 
-const winners = new Set();
+const winnersJSON = window.localStorage.getItem("winners");
+const winners = winnersJSON ? JSON.parse(winnersJSON) : [];
 
 document.querySelector("#app").innerHTML = `
   <div>
     <form>
       <input type="number" name="candidate" />
-    </form>
-    <div id="status"></div>
-    <div class="buttons">
       <input type="button" value="1" />
       <input type="button" value="2" />
       <input type="button" value="3" />
@@ -18,28 +16,43 @@ document.querySelector("#app").innerHTML = `
       <input type="button" value="7" />
       <input type="button" value="8" />
       <input type="button" value="9" />
+      <button id="undo">&lt;</button>
       <input type="button" value="0" />
-    </div>
+      <input type="submit" value=">" />
+    </form>
+    <div id="status"></div>
+    
   </div>
 `;
 
 const form = document.querySelector("form");
+const undo = document.querySelector("#undo");
 const status = document.querySelector("#status");
 
 form.addEventListener("submit", (ev) => {
   ev.preventDefault();
   const candidate = form.elements.candidate.value;
-  if (winners.has(candidate)) {
+  if (winners.includes(candidate)) {
     status.innerHTML = "already won";
   } else {
-    winners.add(candidate);
+    winners.push(candidate);
     status.innerHTML = "new winner";
+    window.localStorage.setItem("winners", JSON.stringify(winners));
+  }
+  form.elements.candidate.value = "";
+});
+
+undo.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  if (form.elements.candidate.value) {
+    form.elements.candidate.value = "";
+  } else {
+    winners.splice(winners.length - 1);
+    window.localStorage.setItem("winners", JSON.stringify(winners));
   }
 });
 
-const numberButtons = document.querySelectorAll(
-  '.buttons > input[type="button"]'
-);
+const numberButtons = document.querySelectorAll('input[type="button"]');
 numberButtons.forEach((button) => {
   button.addEventListener("click", (ev) => {
     const number = ev.target.value;
